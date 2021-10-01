368a21cb5c0c91530a3929b04e93c960c2bbee1b
riyanris <riyanrisky129@gmail.com>
2021-10-01 09:45:24 +0700

    update data source

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -2,7 +2,7 @@ class DataSource {
   static searchMeals(keyword) {
     return fetch(
       `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`
-    )
+    ) 
       .then((response) => {
         return response.json();
       })
